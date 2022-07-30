639a376fc009b41fd12293e0d1efbadcf3e574a5
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-07-30 08:46:22 -0400

    se termina con los handler del controller de cursos, putMapping y deleteMapping

--- msvc-cursos/src/main/java/org/supanta/springcloud/msvc/cursos/controllers/CursoController.java
+++ msvc-cursos/src/main/java/org/supanta/springcloud/msvc/cursos/controllers/CursoController.java
@@ -1,10 +1,9 @@
 package org.supanta.springcloud.msvc.cursos.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.supanta.springcloud.msvc.cursos.entity.Curso;
 import org.supanta.springcloud.msvc.cursos.services.CursoService;
 
@@ -31,6 +30,32 @@ public class CursoController {
     return ResponseEntity.notFound().build();             // Cuando no exista el curso un 404
   }
 
+  @PostMapping()
+  public ResponseEntity<?> crear(@RequestBody Curso curso){                     // solicita un curso al json del cliente
+    Curso cursoDb = service.guardar(curso);                                     // guarda en una variable la query junto al curso del client
+    return ResponseEntity.status(HttpStatus.CREATED).body(cursoDb);             // retorna un 201 y el curso creado
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> editar(@PathVariable Long id, @RequestBody Curso curso){ //obtiene el valor id y el curso que viene en el json
+    Optional<Curso> cursoOP = service.porId(id);                                    // guarda el curso que consigue con la query
+    if(cursoOP.isPresent()){                                                        // si viene un curso se ejecuta
+      Curso cursoDB = cursoOP.get();                                                // guarda el curso de la bs en una var
+      cursoDB.setNombre(curso.getNombre());                                         // setea el nombre, con el valor del curso que viene
+      return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDB)); //retorna un 200 y hace la query con el curso seteado
+    }
+    return ResponseEntity.notFound().build();                                        // si no hay curso, devuelve un 404
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> eliminar(@PathVariable Long id){
+    Optional<Curso> cursoOp = service.porId(id);
+    if(cursoOp.isPresent()){
+      service.eliminar(cursoOp.get().getId());
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
+  }
 
 
 }
